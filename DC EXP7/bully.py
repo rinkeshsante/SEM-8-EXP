b7c9e9244175ad3bda39f7d9d6310bce093ef853
Rinkesh <rinkoosante@gmail.com>
2022-04-25 18:28:59 +0530

    tried bully algo

---
+++ DC EXP7/bully.py
@@ -0,0 +1,24 @@
+coordinator = 0
+n = int(input(("Enter the number of process: ")))
+status = []
+priority = []
+
+
+def Bulley(initiator):
+    global coordinator
+    initiator = initiator - 1
+    coordinator = initiator + 1
+    for i in range(n):
+        if priority[initiator] < priority[i]:
+            print(f"Election message is sent from {initiator+1} to {i+1}")
+        if status[i] == 1:
+            Bulley(i + 1)
+
+
+for i in range(n):
+    print(f"For Process {i+1}:")
+    status.append(int(input("Status: ")))
+    priority.append(int(input("Priority: ")))
+initiator = int(input("Which process will initiate election? "))
+Bulley(initiator)
+print(f"Final coordinator: {coordinator}")
--- DC_EXP7/bully.py
+++
@@ -1,8 +0,0 @@
-
-
-class node:
-    def __init__(self, id) -> None:
-        self.id = id
-
-    def checkElection(self, request_id):
-        pass
